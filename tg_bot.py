37b19c9f1ea98eb9f4b596796da52f369c595c33
paraigor <103633539+paraigor@users.noreply.github.com>
2025-02-02 14:02:45 +0300

    Add logging

--- tg_bot.py
+++ tg_bot.py
@@ -1,3 +1,4 @@
+import logging
 import random
 from pathlib import Path
 
@@ -12,20 +13,29 @@ from telegram.ext import (
     Updater,
 )
 
+from log_handler import TgLogHandler
 from tools import parse_text
+
+logger = logging.getLogger(__file__)
 
 
 def start(update: Update, context):
     chat_id_str = str(update.effective_chat.id)
     reply_keyboard = [["Новый вопрос", "Сдаться"], ["Мой счёт"]]
-    update.message.reply_text(
-        "Приветствуем на нашей викторине! Для начала нажми [Новый вопрос]",
-        reply_markup=ReplyKeyboardMarkup(
-            reply_keyboard,
-            resize_keyboard=True,
-            is_persistent=True,
-        ),
-    )
+
+    try:
+        update.message.reply_text(
+            "Приветствуем на нашей викторине!\nДля начала нажми [Новый вопрос]",
+            reply_markup=ReplyKeyboardMarkup(
+                reply_keyboard,
+                resize_keyboard=True,
+                is_persistent=True,
+            ),
+        )
+    except Exception as err:
+        logger.info("Бот Телаграм упал с ошибкой:")
+        logger.error(err)
+
     db_connection.set(chat_id_str, "")
 
 
@@ -33,13 +43,17 @@ def handle_new_question_request(update: Update, context):
     chat_id_str = str(update.effective_chat.id)
     question = random.choice(list(qa_set.keys()))
     db_connection.set(chat_id_str, question)
-    update.message.reply_text(question)
+    try:
+        update.message.reply_text(question)
+    except Exception as err:
+        logger.info("Бот Телаграм упал с ошибкой:")
+        logger.error(err)
 
     return "SOLUTION_ATTEMPT"
 
 
 def handle_solution_attempt(update: Update, context):
-    text = update.message.text.replace("\n", " ").strip().lower()
+    message = update.message.text.replace("\n", " ").strip().lower()
     chat_id_str = str(update.effective_chat.id)
     question = db_connection.get(chat_id_str)
 
@@ -48,35 +62,50 @@ def handle_solution_attempt(update: Update, context):
         answer = answer.lower()
         short_answer = answer.split(".")[0].split("(")[0].strip().lower()
 
-    if text == short_answer or text == answer:
-        update.message.reply_text(
-            "Правильно! Поздравляю! Для следующего вопроса нажми [Новый вопрос]"
-        )
+    if message == short_answer or message == answer:
+        try:
+            update.message.reply_text(
+                "Правильно! Поздравляю!\nДля следующего вопроса нажми [Новый вопрос]"
+            )
+        except Exception as err:
+            logger.info("Бот Телаграм упал с ошибкой:")
+            logger.error(err)
         db_connection.set(chat_id_str, "")
 
         return ConversationHandler.END
     else:
-        update.message.reply_text("Неправильно… Попробуешь ещё раз?")
+        try:
+            update.message.reply_text("Неправильно… Попробуешь ещё раз?")
+        except Exception as err:
+            logger.info("Бот Телаграм упал с ошибкой:")
+            logger.error(err)
 
 
 def handle_giveup_request(update: Update, context):
     chat_id_str = str(update.effective_chat.id)
     question = db_connection.get(chat_id_str)
     answer = qa_set.get(question)
-    update.message.reply_text(
-        f"Правильный ответ: {answer}"
-    )
+    try:
+        update.message.reply_text(f"Правильный ответ: {answer}")
+    except Exception as err:
+        logger.info("Бот Телаграм упал с ошибкой:")
+        logger.error(err)
     db_connection.set(chat_id_str, "")
 
     handle_new_question_request(update, context)
 
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        level=logging.INFO,
+    )
+
     env = Env()
     env.read_env()
 
     tg_bot_token = env.str("TG_BOT_TOKEN")
-    chat_id = env("TG_CHAT_ID")
+    admin_chat_id = env("TG_CHAT_ID")
     db_host = env.str("REDIS_DB_HOST")
     db_port = env.int("REDIS_DB_PORT")
     db_user = env.str("REDIS_DB_USERNAME", "default")
@@ -112,13 +141,16 @@ if __name__ == "__main__":
                     MessageHandler(
                         Filters.text,
                         handle_solution_attempt,
-                    )
+                    ),
                 ],
             },
             fallbacks=[],
         )
     )
     dispatcher.add_handler(CommandHandler("start", start))
+
+    logger.addHandler(TgLogHandler(updater.bot, admin_chat_id))
+    logger.info("Бот Телаграм запущен")
 
     updater.start_polling()
     updater.idle()
--- vk_bot.py
+++ vk_bot.py
@@ -1,14 +1,19 @@
+import logging
 import random
 from pathlib import Path
 
 import redis
+import telegram
 import vk_api
 from environs import Env
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from vk_api.longpoll import VkEventType, VkLongPoll
 from vk_api.utils import get_random_id
 
+from log_handler import TgLogHandler
 from tools import parse_text
+
+logger = logging.getLogger(__file__)
 
 
 def start(event, vk_api):
@@ -19,12 +24,16 @@ def start(event, vk_api):
     keyboard.add_line()
     keyboard.add_button("Мой счёт", color=VkKeyboardColor.SECONDARY)
 
-    vk_api.messages.send(
-        user_id=user_id,
-        message="Приветствуем на нашей викторине! Для начала нажми [Новый вопрос]",
-        keyboard=keyboard.get_keyboard(),
-        random_id=get_random_id(),
-    )
+    try:
+        vk_api.messages.send(
+            user_id=user_id,
+            message="Приветствуем на нашей викторине!\nДля начала нажми [Новый вопрос]",
+            keyboard=keyboard.get_keyboard(),
+            random_id=get_random_id(),
+        )
+    except Exception as err:
+        logger.info("Бот VK упал с ошибкой:")
+        logger.error(err)
     db_connection.set(user_id, "")
 
 
@@ -32,15 +41,20 @@ def handle_new_question_request(event, vk_api):
     user_id = event.user_id
     question = random.choice(list(qa_set.keys()))
     db_connection.set(user_id, question)
-    vk_api.messages.send(
-        user_id=user_id,
-        message=question,
-        random_id=get_random_id(),
-    )
+
+    try:
+        vk_api.messages.send(
+            user_id=user_id,
+            message=question,
+            random_id=get_random_id(),
+        )
+    except Exception as err:
+        logger.info("Бот VK упал с ошибкой:")
+        logger.error(err)
 
 
 def handle_solution_attempt(event, vk_api):
-    text = event.text.replace("\n", " ").strip().lower()
+    message = event.text.replace("\n", " ").strip().lower()
     user_id = event.user_id
     question = db_connection.get(user_id)
 
@@ -49,41 +63,65 @@ def handle_solution_attempt(event, vk_api):
         answer = answer.lower()
         short_answer = answer.split(".")[0].split("(")[0].strip().lower()
 
-    if text == short_answer or text == answer:
-        vk_api.messages.send(
-            user_id=user_id,
-            message="Правильно! Поздравляю! Для следующего вопроса нажми [Новый вопрос]",
-            random_id=get_random_id(),
-        )
+    if message == short_answer or message == answer:
+        try:
+            vk_api.messages.send(
+                user_id=user_id,
+                message="Правильно! Поздравляю!\nДля следующего вопроса нажми [Новый вопрос]",
+                random_id=get_random_id(),
+            )
+        except Exception as err:
+            logger.info("Бот VK упал с ошибкой:")
+            logger.error(err)
         db_connection.set(user_id, "")
     else:
-        vk_api.messages.send(
-            user_id=user_id,
-            message="Неправильно… Попробуешь ещё раз?",
-            random_id=get_random_id(),
-        )
+        try:
+            vk_api.messages.send(
+                user_id=user_id,
+                message="Неправильно… Попробуешь ещё раз?",
+                random_id=get_random_id(),
+            )
+        except Exception as err:
+            logger.info("Бот VK упал с ошибкой:")
+            logger.error(err)
 
 
 def handle_giveup_request(event, vk_api):
     user_id = event.user_id
     question = db_connection.get(user_id)
     answer = qa_set.get(question)
-    vk_api.messages.send(
-        user_id=user_id,
-        message=f"Правильный ответ: {answer}\nДля следующего вопроса нажми [Новый вопрос]",
-        random_id=get_random_id(),
-    )
+    try:
+        vk_api.messages.send(
+            user_id=user_id,
+            message=f"Правильный ответ: {answer}\nДля следующего вопроса нажми [Новый вопрос]",
+            random_id=get_random_id(),
+        )
+    except Exception as err:
+        logger.info("Бот VK упал с ошибкой:")
+        logger.error(err)
     db_connection.set(user_id, "")
 
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(message)s",
+        level=logging.INFO,
+    )
+
     env = Env()
     env.read_env()
+
     vk_token = env.str("VK_GROUP_TOKEN")
     db_host = env.str("REDIS_DB_HOST")
     db_port = env.int("REDIS_DB_PORT")
     db_user = env.str("REDIS_DB_USERNAME", "default")
     db_pass = env.str("REDIS_DB_PASSWORD")
+    bot_token = env("TG_BOT_TOKEN")
+    admin_chat_id = env("TG_CHAT_ID")
+
+    bot = telegram.Bot(bot_token)
+    logger.addHandler(TgLogHandler(bot, admin_chat_id))
+    logger.info("Бот VK запущен")
 
     db_connection = redis.Redis(
         host=db_host,
@@ -96,9 +134,13 @@ if __name__ == "__main__":
     question_file = Path("questions/1vs1200.txt")
     qa_set = parse_text(question_file)
 
-    vk_session = vk_api.VkApi(token=vk_token)
-    vk_api = vk_session.get_api()
-    longpoll = VkLongPoll(vk_session)
+    try:
+        vk_session = vk_api.VkApi(token=vk_token)
+        vk_api = vk_session.get_api()
+        longpoll = VkLongPoll(vk_session)
+    except Exception as err:
+        logger.info("Бот VK упал с ошибкой:")
+        logger.error(err)
 
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
